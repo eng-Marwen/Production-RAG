cec0504d072a3e196f76071ba3e50a5f57e1bd40
Marwen Boussabat <boussabatmarwen9@gmail.com>
2026-07-09 09:41:06 +0100

    Refactor main.py to import ragResponse and streamline document loading comments

--- simple prototype/main.py
+++ simple prototype/main.py
@@ -2,12 +2,14 @@ from dotenv import load_dotenv
 from importlib.metadata import version
 import os
 from loadAndChunk import LoadAndChunk
-from embeddings import chunksToEmbeddings
+from embeddings import chunksToEmbeddings, ragResponse
+# from embeddings import ragResponse
 load_dotenv()
 if __name__ == "__main__":
     # Load and chunk the documents
-    chunks = LoadAndChunk("./Document 8.pdf")
-    print(chunks[0])
-    # Create embeddings for the chunks
-    #embeddings = chunksToEmbeddings(chunks)
-
+    # chunks = LoadAndChunk("./Document 8.pdf")
+    # # Create embeddings for the chunks and save them to a vector database
+    # embeddings = chunksToEmbeddings(chunks)
+    # print(embeddings)
+    response=ragResponse("give me the main sql syntax")
+    print(response)
